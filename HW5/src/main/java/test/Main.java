88e41295503a1a2a9755ecee53855ede1aeb597f
JoJunHwa <jjj5306@naver.com>
2024-11-15 16:58:49 +0900

    feat : add database connection

--- HW5/src/main/java/test/Main.java
+++ HW5/src/main/java/test/Main.java
@@ -1,7 +1,32 @@
 package test;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String args[]) {
+        try {
+            Properties props = new Properties();
+            props.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
+
+            String url = props.getProperty("url");
+            String username = props.getProperty("username");
+            String password = props.getProperty("password");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            Statement stmt = con.createStatement();
+
+            //
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Book");
+
+            while(rs.next())
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+
+            con.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
